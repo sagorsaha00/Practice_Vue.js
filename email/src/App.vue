61091c15ae0a0cfea319586ccf339ec331e9a15a
Sagor saha <117963424+sagorsaha00@users.noreply.github.com>
2023-10-14 15:58:54 +0600

    email file added

---
+++ email/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app" class="d-flex justify-content-center" :class="hi">
+    <div class=" col-md-6 mt-3">
+      <div class="card">
+        <div class="card-header">Newsletter</div>
+        <div class="card-body">
+          <form @submit.prevent="submit">
+            <div class="mb-2">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Name"
+                 :value="newsletterName"
+                 @input="newsletterName = $event.target.value"
+              />
+            </div>
+            <div class="mb-2">
+              <!-- <emailinput v-model="newsletterName" /> -->
+              <emailinput v-model="newsletterName" />
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import emailinput from './components/Emailinput.vue'
+export default {
+  name: "App",
+  components:{
+     emailinput
+  },
+  data() {
+    return {
+      newsletterName: "",
+      newsletterEmail: "",
+    };
+  },
+  methods: {
+    submit() {
+      console.log("Newletter Submitted", {
+        name: this.newsletterName,
+        email: this.newsletterEmail,
+      });
+    },
+  },
+};
+</script>
+
+<style src="bootstrap/dist/css/bootstrap.css">
+.hi{
+  height: 300px;
+  width:300px;
+}
+</style>
---
+++ email/src/components/Emailinput.vue
@@ -0,0 +1,31 @@
+<template>
+  <input
+   
+                type="email"
+                class="form-control"
+                placeholder="E-mail"
+                :value="modalValue"
+                @input="update($event)"
+                :class="{
+                  'is-valid':
+                  modalValue.length >= 4 && modalValue.length > 0,
+                  'is-invalid':
+                  modalValue.length < 4 && modalValue.length > 0,
+                }"
+              />
+    </template>
+
+    <script>
+// import { toValue } from 'vue';
+
+export default {
+    name:'Emailinput',
+   props:['modalValue'],
+   methods:{
+    update($event) {
+        this.$emit('update:modalValue', $event.target.value)
+    }
+   }
+}
+
+</script>
